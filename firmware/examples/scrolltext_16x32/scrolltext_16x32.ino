92b1b666cf7ddadb7ac9370591d534eb24ef0a85
Paul Kourany <pkourany@users.noreply.github.com>
2014-07-14 15:43:29 -0400

    Update scrolltext_16x32.ino

--- firmware/examples/scrolltext_16x32/scrolltext_16x32.ino
+++ firmware/examples/scrolltext_16x32/scrolltext_16x32.ino
@@ -7,8 +7,8 @@
 // BSD license, all text above must be included in any redistribution.
 
 #if defined(SPARK)
-#include "../Adafruit_mfGFX/Adafruit_mfGFX.h"   // Core graphics library
-#include "RGBmatrixPanel.h" // Hardware-specific library
+#include "Adafruit_mfGFX/Adafruit_mfGFX.h"   // Core graphics library
+#include "RGBmatrixPanel/RGBmatrixPanel.h" // Hardware-specific library
 #include "math.h"
 #else
 #include <avr/pgmspace.h>
